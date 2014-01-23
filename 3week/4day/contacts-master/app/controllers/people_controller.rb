43c2294da5efaa4fc694c023c8317b96adb8ffe1
smspezzano <smspezzano@gmail.com>
2014-01-23 11:16:54 -0800

    added W3D4 ML

---
+++ 3week/4day/contacts-master/app/controllers/people_controller.rb
@@ -0,0 +1,15 @@
+class PeopleController < ApplicationController
+  def index
+    # remember the domain name selected
+    @email_domain = params[:email_domain] || 'All'
+
+    # get all people
+    @people = Person.all
+
+    # get all the available domain names
+    @email_domains = @people.map { |p| p.email.split('@').last }.uniq.sort
+
+    # filter the people based on domain name, when provided
+    @people.to_a.select! { |p| p.email.index @email_domain } unless @email_domain == 'All'
+  end
+end
