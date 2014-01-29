1ae4df3e2c1948d9e5d01bb30b4062e1e10d7d93
smspezzano <smspezzano@gmail.com>
2014-01-29 13:34:00 -0800

    morning lecture work

--- 4wk/2dy/lab/store/app/controllers/categories_controller.rb
+++ 4wk/2dy/lab/store/app/controllers/categories_controller.rb
@@ -10,7 +10,7 @@ class CategoriesController < ApplicationController
 	def create
 		new_category = params.require(:category).permit(:name)
 		category = Category.create(new_category)
-		redirect_to categories_path 
+		redirect_to category_path(category.id)    
 	end
 
 	def show
